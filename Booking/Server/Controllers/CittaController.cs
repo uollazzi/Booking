32f302bf2d6a5f1b75596acce7a9a121ffe6c368
Matteo Valenzi <matteo.valenzi@outlook.com>
2022-11-21 15:14:26 +0100

    controller citta

---
+++ Booking/Server/Controllers/CittaController.cs
@@ -0,0 +1,17 @@
+﻿using Booking.Shared.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Booking.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CittaController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IEnumerable<CittaModel>> GetAll()
+        {
+            return new List<CittaModel>();
+        }
+    }
+}
