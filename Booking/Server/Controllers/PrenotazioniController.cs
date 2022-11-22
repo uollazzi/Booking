1c3dd8b62d2f5acc49a2cf1080e34cc3761e752f
Matteo Valenzi <matteo.valenzi@outlook.com>
2022-11-22 12:04:01 +0100

    verifica ok

--- Booking/Server/Controllers/PrenotazioniController.cs
+++ Booking/Server/Controllers/PrenotazioniController.cs
@@ -26,10 +26,10 @@ namespace Booking.Server.Controllers
             var al = DateTimeOffset.FromUnixTimeSeconds(model.Al).LocalDateTime;
 
             var struttura = await _dc.Strutture.AsNoTracking()
-                                               .Include(x => x.Stanze)
-                                               .ThenInclude(x => x.Disponibilita)
-                                               .Include(x => x.Stanze)
-                                               .ThenInclude(x => x.Prenotazioni)
+                                               .Include(x => x.Stanze!)
+                                               .ThenInclude(x => x.Disponibilita!)
+                                               .Include(x => x.Stanze!)
+                                               .ThenInclude(x => x.Prenotazioni!)
                                                .SingleOrDefaultAsync(x => x.Id == model.StrutturaId);
 
             if (struttura == null) return NotFound("Struttura non trovata");
@@ -37,11 +37,14 @@ namespace Booking.Server.Controllers
             try
             {
                 // cerco se ci sono stanze prenotabili nella struttura
-                
-                var stanzePrenotabili = struttura.Stanze
+                // in base al periodo ed in base al numero di posti
+                var stanzePrenotabili = struttura.Stanze!
                     .Where(x => x.Disponibilita!.Any(a => a.Dal <= dal && a.Al >= al) && x.Capienza >= model.NumeroPosti);
 
-                var stanza = stanzePrenotabili.FirstOrDefault(x => !x.Prenotazioni.Any(x => x.Dal < al || x.Al > dal));
+                // delle stanze di cui sopra
+                // ritorno la prima disponibile
+                // (cioè quella che ha tutte le prenotazioni che non si accavallano al periodo selezionato)
+                var stanza = stanzePrenotabili.FirstOrDefault(x => x.Prenotazioni!.All(x => x.Dal >= al || x.Al <= dal));
 
                 return stanza != null;
             }
