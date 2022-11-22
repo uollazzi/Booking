c04f6be5d64e6b5f601fa42192a2650426cada51
Matteo Valenzi <matteo.valenzi@outlook.com>
2022-11-22 09:19:22 +0100

    VerificaDisponibilitaModel

---
+++ Booking/Server/Controllers/PrenotazioniController.cs
@@ -0,0 +1,17 @@
+﻿using Booking.Shared.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Booking.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PrenotazioniController : ControllerBase
+    {
+        [HttpPost("verifica")]
+        public async Task<ActionResult<bool>> Verifica(VerificaDisponibilitaModel model)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Booking/Shared/Models/VerificaDisponibilitaModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booking.Shared.Models
+{
+    public class VerificaDisponibilitaModel
+    {
+        public long Dal { get; set; } // epoch
+        public long Al { get; set; } // epoch
+        public int NumeroPosti { get; set; }
+        public int StrutturaId { get; set; }
+    }
+}
